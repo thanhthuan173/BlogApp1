aa18d74a574f603d2589351ac580a62417e09bce
Pham Thanh Thuan <thanhthuan009753@gmail.com>
2024-07-10 01:53:50 +0700

    them avatar trong profile

--- app/src/main/java/com/example/blogapp1/fragments/Profile.java
+++ app/src/main/java/com/example/blogapp1/fragments/Profile.java
@@ -2,15 +2,19 @@ package com.example.blogapp1.fragments;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.Manifest;
 import android.content.Intent;
-import android.media.Image;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -42,7 +46,6 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.auth.User;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -55,6 +58,8 @@ import java.util.Map;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends Fragment {
+
+    private static final int REQUEST_CODE = 101;
 
     private TextView nameTv, toolbarNameTv, statusTv, followingCountTv, followersCountTv, postCountTv;
     private CircleImageView profileImage;
@@ -103,17 +108,26 @@ public class Profile extends Fragment {
         editProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                CropImage.activity()
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .setAspectRatio(1,1)
-                        .start(getContext(),Profile.this);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_MEDIA_IMAGES)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_MEDIA_IMAGES}, REQUEST_CODE);
+                    } else {
+                        openImagePicker();
+                    }
+                } else {
+                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+                    } else {
+                        openImagePicker();
+                    }
+                }
             }
         });
     }
 
     private void init(View view) {
-
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         assert getActivity() != null;
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
@@ -130,32 +144,26 @@ public class Profile extends Fragment {
         countLayout = view.findViewById(R.id.countLayout);
         editProfileBtn = view.findViewById(R.id.edit_profileImages);
 
-
         FirebaseAuth auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
-
     }
 
     private void loadBasicData() {
-
         DocumentReference userRef = FirebaseFirestore.getInstance().collection("Users")
                 .document(user.getUid());
 
         userRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-
                 if (error != null)
                     return;
 
                 assert value != null;
                 if (value.exists()) {
-
                     String name = value.getString("name");
                     String status = value.getString("status");
                     int followers = value.getLong("followers").intValue();
                     int following = value.getLong("following").intValue();
-
                     String profileURL = value.getString("profileImage");
 
                     nameTv.setText(name);
@@ -164,27 +172,27 @@ public class Profile extends Fragment {
                     followersCountTv.setText(String.valueOf(followers));
                     followingCountTv.setText(String.valueOf(following));
 
-                    Glide.with(getContext().getApplicationContext())
-                            .load(profileURL)
-                            .placeholder(R.drawable.ic_person)
-                            .timeout(6500)
-                            .into(profileImage);
+                    if (getContext() != null) {
+                        Glide.with(getContext().getApplicationContext())
+                                .load(profileURL)
+                                .placeholder(R.drawable.ic_person)
+                                .timeout(6500)
+                                .into(profileImage);
+                    }
                 }
-
             }
         });
     }
 
-    private void loadPostImages(){
 
-        if(isMyProfile){
+    private void loadPostImages() {
+        if (isMyProfile) {
             uid = user.getUid();
-        }else {
-
+        } else {
+            // Handle other user profile case
         }
 
         DocumentReference reference = FirebaseFirestore.getInstance().collection("Users").document(uid);
-
         Query query = reference.collection("Images");
 
         FirestoreRecyclerOptions<PostImageModel> options = new FirestoreRecyclerOptions.Builder<PostImageModel>()
@@ -195,63 +203,85 @@ public class Profile extends Fragment {
             @NonNull
             @Override
             public PostImageHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.profile_image_items,parent,false);
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.profile_image_items, parent, false);
                 return new PostImageHolder(view);
             }
 
             @Override
             protected void onBindViewHolder(@NonNull PostImageHolder holder, int position, @NonNull PostImageModel model) {
-
                 Glide.with(holder.itemView.getContext().getApplicationContext())
                         .load(model.getImageUrl())
                         .timeout(6500)
                         .into(holder.imageView);
             }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public void onDataChanged() {
+                // Handle changes in data
+                adapter.notifyDataSetChanged();
+            }
         };
+
+        adapter.setHasStableIds(true);
+        recyclerView.setAdapter(adapter);
     }
 
-    private static class PostImageHolder extends RecyclerView.ViewHolder{
-
+    private static class PostImageHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
-        public PostImageHolder(@NonNull View itemView){
-            super(itemView);
 
+        public PostImageHolder(@NonNull View itemView) {
+            super(itemView);
             imageView = itemView.findViewById(R.id.imageView);
         }
     }
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
         adapter.startListening();
     }
 
     @Override
-    public void onStop(){
+    public void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    private void openImagePicker() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1, 1)
+                .start(getContext(), Profile.this);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
-
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             Uri uri = result.getUri();
-            uploadImage(uri);
+            if (uri != null) {
+                uploadImage(uri);
+            } else {
+                Toast.makeText(getContext(), "Failed to get the cropped image", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    private void uploadImage(Uri uri){
-        StorageReference reference = FirebaseStorage.getInstance().getReference().child("Profile Images");
+    private void uploadImage(Uri uri) {
+        StorageReference reference = FirebaseStorage.getInstance().getReference().child("Profile Images").child(user.getUid() + ".jpg");
 
         reference.putFile(uri)
                 .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                        if (task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             reference.getDownloadUrl()
                                     .addOnSuccessListener(new OnSuccessListener<Uri>() {
                                         @Override
@@ -260,32 +290,63 @@ public class Profile extends Fragment {
                                             UserProfileChangeRequest.Builder request = new UserProfileChangeRequest.Builder();
                                             request.setPhotoUri(uri);
 
-                                            user.updateProfile(request.build());
-
-                                            Map<String, Object> map = new HashMap<>();
-                                            map.put("profileImage", imageURL);
-
-                                            FirebaseFirestore.getInstance().collection("Users")
-                                                    .document(user.getUid())
-                                                    .update(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            user.updateProfile(request.build())
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
                                                         @Override
                                                         public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                Map<String, Object> map = new HashMap<>();
+                                                                map.put("profileImage", imageURL);
 
-                                                            if(task.isSuccessful()){
-                                                                Toast.makeText(getContext(), "Updated Successful", Toast.LENGTH_SHORT).show();
-                                                            }else{
-                                                                Toast.makeText(getContext(), "Error: " +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                                                FirebaseFirestore.getInstance().collection("Users")
+                                                                        .document(user.getUid())
+                                                                        .update(map)
+                                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                            @Override
+                                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                                if (task.isSuccessful()) {
+                                                                                    Toast.makeText(getContext(), "Updated Successfully", Toast.LENGTH_SHORT).show();
+                                                                                    // Cập nhật hình ảnh vào profileImage
+                                                                                    if (getContext() != null) {
+                                                                                        Glide.with(getContext().getApplicationContext())
+                                                                                                .load(imageURL)
+                                                                                                .placeholder(R.drawable.ic_person)
+                                                                                                .timeout(6500)
+                                                                                                .into(profileImage);
+                                                                                    }
+                                                                                } else {
+                                                                                    Toast.makeText(getContext(), "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                                                                }
+                                                                            }
+                                                                        });
+                                                            } else {
+                                                                Toast.makeText(getContext(), "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                                             }
-
                                                         }
                                                     });
                                         }
                                     });
-                        }else{
-                            Toast.makeText(getContext(), "Error: " +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openImagePicker();
+            } else {
+                Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
+
+
